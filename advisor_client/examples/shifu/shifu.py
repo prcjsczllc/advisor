0db44192ad947a86072c49e206fb07bc2151ce8d
licliu <licliu@paypal.com>
2019-01-10 00:17:27 -0800

    shifu example

--- advisor_client/examples/shifu/shifu.py
+++ advisor_client/examples/shifu/shifu.py
@@ -3,11 +3,12 @@
 import argparse
 import subprocess
 import json
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-LearningRate", type=float, default=0.1)
 parser.add_argument("-NumHiddenNodes", type=float, default=10)
-parser.add_argument("-ActivationFunc", type=str, default="tanh")
+parser.add_argument("-ActivationFunc", type=str, default="Tanh")
 args = parser.parse_args()
 
 
@@ -17,24 +18,28 @@ def main():
     # NumHiddenNodes = args.NumHiddenNodes
     # ActivationFunc = args.ActivationFunc
 
+    # get path
+    parentPath = os.getcwd()
+    shifuPath = parentPath + "/advisor_client/examples/shifu/autoMLTest"
+
     # Change model config
-    with open("./autoMLTest/ModelConfig.json","r+") as modelConfigFile:
+    with open(shifuPath+"/ModelConfig.json","r+") as modelConfigFile:
         modelConfig = json.loads(modelConfigFile.read())
         for arg, value in vars(args).items():
-            modelConfig["train"]["params"][arg] = value
+            modelConfig["train"]["params"][arg] = [value]
 
-    with open("./autoMLTest/ModelConfig.json", "wt") as t:
-      json.dump(modelConfig,t,indent=2,sort_keys=True)
+    with open(shifuPath+"/ModelConfig.json", "wt") as t:
+        json.dump(modelConfig,t,indent=2,sort_keys=True)
 
     # Compute or learning
-    subprocess.check_call(["shifu train>train.log"])
+    subprocess.check_call(["cd " + shifuPath + "&& bash shifu train && > train.log"],shell=True)
 
     # Output the metrics
-    logFileDir = "./advisor_client/examples/shifu/autoMLTest/train.log"
+    logFileDir = shifuPath + "/train.log"
     lines = [line.rsplit(' Validation Error: ')[1][:8] for line in open(logFileDir) if "Epoch #" in line and "Train Error:" in line and "Validation Error:" in line]
     validationError = min(lines)
 
     print(validationError)
 
 if __name__ == "__main__":
-  main()
+    main()
