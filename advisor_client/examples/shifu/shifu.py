b17c968ca956bfaded6e140e90b3924335456e42
licliu <licliu@paypal.com>
2019-01-10 00:21:31 -0800

    edit path

--- advisor_client/examples/shifu/shifu.py
+++ advisor_client/examples/shifu/shifu.py
@@ -23,19 +23,19 @@ def main():
     shifuPath = parentPath + "/advisor_client/examples/shifu/autoMLTest"
 
     # Change model config
-    with open(shifuPath+"/ModelConfig.json","r+") as modelConfigFile:
+    with open("./ModelConfig.json","r+") as modelConfigFile:
         modelConfig = json.loads(modelConfigFile.read())
         for arg, value in vars(args).items():
             modelConfig["train"]["params"][arg] = [value]
 
-    with open(shifuPath+"/ModelConfig.json", "wt") as t:
+    with open("./ModelConfig.json", "wt") as t:
         json.dump(modelConfig,t,indent=2,sort_keys=True)
 
     # Compute or learning
-    subprocess.check_call(["cd " + shifuPath + "&& bash shifu train && > train.log"],shell=True)
+    subprocess.check_call(["cd " + "./autoMLTest" + "&& bash shifu train && > train.log"],shell=True)
 
     # Output the metrics
-    logFileDir = shifuPath + "/train.log"
+    logFileDir =  + "./train.log"
     lines = [line.rsplit(' Validation Error: ')[1][:8] for line in open(logFileDir) if "Epoch #" in line and "Train Error:" in line and "Validation Error:" in line]
     validationError = min(lines)
 
