a4a2654c7b40747defbc2b73893e74373ef2e7e0
licliu <licliu@paypal.com>
2019-01-09 16:54:29 -0800

    add shifu example

--- advisor_client/examples/shifu/shifu.py
+++ advisor_client/examples/shifu/shifu.py
@@ -2,7 +2,7 @@
 
 import argparse
 import subprocess
-
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-LearningRate", type=float, default=0.1)
@@ -12,17 +12,29 @@ args = parser.parse_args()
 
 
 def main():
-  # Read parameters
-  LearningRate = args.LearningRate
-  NumHiddenNodes = args.NumHiddenNodes
-  ActivationFunc = args.ActivationFunc
+    # Read parameters
+    # LearningRate = args.LearningRate
+    # NumHiddenNodes = args.NumHiddenNodes
+    # ActivationFunc = args.ActivationFunc
 
-  # Compute or learning
-  subprocess.check_call(["shifu train"])
+    # Change model config
+    with open("./advisor_client/examples/shifu/autoMLTest/ModelConfig.json","r+") as modelConfigFile:
+        modelConfig = json.loads(modelConfigFile.read())
+        for arg, value in vars(args).items():
+            modelConfig["train"]["params"][arg] = value
 
-  # Output the metrics
-  print(y)
+    with open("./advisor_client/examples/shifu/autoMLTest/ModelConfig.json", "wt") as t:
+      json.dump(modelConfig,t,indent=2,sort_keys=True)
 
+    # Compute or learning
+    subprocess.check_call(["shifu train>train.log"])
+
+    # Output the metrics
+    logFileDir = "./advisor_client/examples/shifu/autoMLTest/train.log"
+    lines = [line.rsplit(' Validation Error: ')[1][:8] for line in open(logFileDir) if "Epoch #" in line and "Train Error:" in line and "Validation Error:" in line]
+    validationError = min(lines)
+
+    print(validationError)
 
 if __name__ == "__main__":
   main()
